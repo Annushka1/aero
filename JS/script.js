074e31391bc57eab84dfe5d5f58a1d2c851f27af
Annushka1 <robin1bobin@gmail.com>
2017-04-12 18:55:55 +0300

    плавный скролл

--- JS/script.js
+++ JS/script.js
@@ -11,4 +11,15 @@ $(document).ready(function(){
     autoplayHoverPause:true
    });
 
+  $('a[href*=#]').bind("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+    scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+    });
+    return false;
+
 });
+
+
