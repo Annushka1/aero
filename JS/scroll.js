8124df490777eb8c75e597085db3ef86d086c50f
Annushka1 <robin1bobin@gmail.com>
2017-04-14 18:09:30 +0300

    прокрутка вверх страницы при достижении определенной точки вьюпорта

--- JS/scroll.js
+++ JS/scroll.js
@@ -25,3 +25,50 @@ function numAnimate () {
 //         if (number<=3000) { $('.numb-3000').text(number); };
 //     }, 100);
 // };
+
+var updownElem = document.getElementById('updown');
+
+    var pageYLabel = 0;
+
+    updownElem.onclick = function() {
+      var pageY = window.pageYOffset || document.documentElement.scrollTop;
+
+      switch (this.className) {
+        case 'up':
+          pageYLabel = pageY;
+          window.scrollTo(0, 0);
+          this.className = 'down';
+          break;
+
+        case 'down':
+          window.scrollTo(0, pageYLabel);
+          this.className = 'up';
+      }
+
+    }
+
+    window.onscroll = function() {
+      var pageY = window.pageYOffset || document.documentElement.scrollTop;
+      var innerHeight = document.documentElement.clientHeight;
+
+      switch (updownElem.className) {
+        case '':
+          if (pageY > innerHeight) {
+            updownElem.className = 'up';
+          }
+          break;
+
+        case 'up':
+          if (pageY < innerHeight) {
+            updownElem.className = '';
+          }
+          break;
+
+        case 'down':
+          if (pageY > innerHeight) {
+            updownElem.className = 'up';
+          }
+          break;
+
+      }
+    }
